26bfe258295f53edb0181abc9a697d0f579898be
AleksBest <bokarev.aleks@mail.ru>
2023-10-24 14:43:30 +0300

    Defenfor и Prtogressor

--- Assets/Scenes/Scripts/PiecesScripts/Progressor.cs
+++ Assets/Scenes/Scripts/PiecesScripts/Progressor.cs
@@ -7,10 +7,40 @@ public class Progressor : Piece
     public override List<Vector2Int> GetAvaibleMooves()
     {
         List<Vector2Int> result = new List<Vector2Int>();
-        if (this.y + 1 < board.pieces[x].Length )           //если впереди не край доски
-            if (board.pieces[x][y + 1] == null              //если там нет фигуры
-                || board.pieces[x][y + 1].team != this.team)//или эта фигура вражеская
-                result.Add(new Vector2Int(x, y + 1));       // то туда можно ходить
+
+        for(int i = this.x - 1; i <= this.x + 1; i++)
+        {
+            if (i < 0) continue;                                                                        //левая граница
+            if (i > 8) continue;                                                                        //правая граница
+
+            for (int j = this.y - 1; j <= this.y + 1; j++)
+            {
+                if (j < 0) continue;                                                                    //нижняя граница
+                if (j >= board.pieces[i].Length) continue;                                              //верхняя граница
+
+                if (this.x == i && this.y == j) continue;                                               //клетка с фигурой
+                if (this.x % 2 == 0 && this.y + 1 == j && this.x != i) continue;                        //две лишние клетки сверху
+                if (this.x % 2 == 1 && this.y - 1 == j && this.x != i) continue;                        //две лишние клетки снизу
+
+                if (board.pieces[i][j] != null && board.pieces[i][j].team == this.team) continue;       //есть фигура и она союзная
+
+
+                //перемещение чёрных
+                if (!this.team)
+                {
+                    if (this.x % 2 == 0 && this.y - 1 == j) continue;
+                    if (this.x % 2 == 1 && this.y + 1 != j) continue;
+                }
+                //перемещение белых
+                if (this.team)
+                {
+                    if (this.x % 2 == 0 && this.y - 1 != j) continue;
+                    if (this.x % 2 == 1 && this.y + 1 == j) continue;
+                }
+                result.Add(new Vector2Int(i, j));
+            }
+        }
+
         return result;              
     }
 }
