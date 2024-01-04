b649a73c793c6bef2c0a623613f9d90c8046faf1
Fe1LDr <gendalf232@gmail.com>
2024-01-04 21:25:48 +0300

    АААААААААААААААА

--- Assets/Scenes/Scripts/Computer.cs
+++ Assets/Scenes/Scripts/Computer.cs
@@ -11,25 +11,7 @@ using UnityEngine.UI;
 public class Computer : MonoBehaviour
 {
     private Board board;
-    private List<int> scores = new() { 10, 30, 1000, 90, 40, 50 };
-    private int[,] whiteProgressor = { { 0, 1, 0, 1, 2, 5, 0 }, 
-                                       { 0, 2, 0, 1, 2, 5, -1},
-                                       { 5, 1, 0, 1, 2, 5, 0},
-                                       { 2, 3, 0, 1, 2, 5, -1 },
-                                       { 0, 1, 0, 1, 2, 5, 0},
-                                       { 2, 3, 0, 1, 2, 5, -1},
-                                       { 5, 1, 0, 1, 2, 5, 0 },
-                                       { 0, 2, 0, 1, 2, 5, -1},
-                                       { 0, 1, 0, 1, 2, 5, 0}, };
-    private int[,] blackProgressor = { { 0, 5, 2, 1, 0, 1, 0 },
-                                       { 5, 2, 1, 0, 2, 5, -1},
-                                       { -5, 5, 2, 1, 0, 1, 0},
-                                       { 5, 2, 1, 0, 2, 3, -1},
-                                       { 0, 5, 2, 1, 0, 1, 0},
-                                       { 5, 2, 1, 0, 2, 3, -1},
-                                       { -5, 5, 2, 1, 0, 1, 0},
-                                       { 5, 2, 1, 0, 2, 5, -1},
-                                       { 0, 5, 2, 1, 0, 1, 0}, };
+    private Weights w = new Weights();
     // Start is called before the first frame update
     void Start()
     {
@@ -42,7 +24,7 @@ public class Computer : MonoBehaviour
         if (board.Turn == true && board.AI == true)
         {
             board.AI = false;
-            MinMaxRoot(4, board.pieces, true);
+            MinMaxRoot(3, board.pieces, true);
         }
     }
 
@@ -96,9 +78,8 @@ public class Computer : MonoBehaviour
     public List<int> MinMaxRoot(int depth, Piece[][] pieces, bool isMaximisingPlayer)
     {
         List<List<int>> Moves = GetMoves(pieces, isMaximisingPlayer);
-        int maxscore = -100000;
+        int maxscore = -9999;
         List<int> bestmove = new() { 0, 0, 0, 0 };
-        int ev = EvaluatePosition(pieces);
 
         foreach (List<int> move in Moves)
         {
@@ -117,14 +98,16 @@ public class Computer : MonoBehaviour
             clone[move[2]][move[3]] = clone[move[0]][move[1]];
             clone[move[0]][move[1]] = null;
 
-            int eval = MinMax(depth - 1, clone, -10000, 10000, !isMaximisingPlayer, move);
+            int eval = MinMax(depth - 1, clone, -10000, 10000, !isMaximisingPlayer);
 
             if (eval > maxscore)
             {
                 maxscore = eval;
                 bestmove = move;
             }
+            
         }
+        Debug.LogFormat("{0}, {1}, {2}, {3}, {4}", maxscore, bestmove[0], bestmove[1], bestmove[2], bestmove[3]);
         board.SelectTile(new Vector2Int(bestmove[0], bestmove[1]));
         board.SelectTile(new Vector2Int(bestmove[2], bestmove[3]));
         Button yourButton = GameObject.Find("Yes")?.GetComponent<Button>();
@@ -141,17 +124,17 @@ public class Computer : MonoBehaviour
         return bestmove;
     }
 
-    public int MinMax(int depth, Piece[][] pieces, int alpha, int beta, bool isMaximisingPlayer, List<int> moveLast)
+    public int MinMax(int depth, Piece[][] pieces, int alpha, int beta, bool isMaximisingPlayer)
     {
         if (depth == 0)
         {
-            return newEval(pieces, moveLast);
+            return newEval(pieces);
         }
         List<List<int>> Moves = GetMoves(pieces, isMaximisingPlayer);
 
         if (isMaximisingPlayer)
         {
-            int max_score = -100000;
+            int max_score = -9999;
             foreach (List<int> move in Moves)
             {
                 Piece[][] clone = new Piece[pieces.Length][];
@@ -168,7 +151,7 @@ public class Computer : MonoBehaviour
                 }
                 clone[move[2]][move[3]] = clone[move[0]][move[1]];
                 clone[move[0]][move[1]] = null;
-                max_score = Math.Max(max_score, MinMax(depth - 1, pieces, alpha, beta, !isMaximisingPlayer, move));
+                max_score = Math.Max(max_score, MinMax(depth - 1, pieces, alpha, beta, !isMaximisingPlayer));
                 
                 alpha = Math.Max(alpha, max_score);
                 if (beta <= alpha)
@@ -180,7 +163,7 @@ public class Computer : MonoBehaviour
         }
         else
         {
-            int max_score = 100000;
+            int max_score = 9999;
             foreach (List<int> move in Moves)
             {
                 Piece[][] clone = new Piece[pieces.Length][];
@@ -197,7 +180,7 @@ public class Computer : MonoBehaviour
                 }
                 clone[move[2]][move[3]] = clone[move[0]][move[1]];
                 clone[move[0]][move[1]] = null;
-                max_score = Math.Min(max_score, MinMax(depth - 1, clone, alpha, beta, !isMaximisingPlayer, move));
+                max_score = Math.Min(max_score, MinMax(depth - 1, clone, alpha, beta, !isMaximisingPlayer));
                 
                 beta = Math.Min(beta, max_score);
                 if (beta <= alpha)
@@ -209,17 +192,14 @@ public class Computer : MonoBehaviour
         }
     }
 
-    public int newEval(Piece[][] pieces, List<int> move)
+    public int newEval(Piece[][] pieces)
     {
-        
-
         return EvaluatePosition(pieces);
     }
 
     public int EvaluatePosition(Piece[][] pieces)
     {
-        int whiteEvaluation = 0;
-        int blackEvaluation = 0;
+        int total = 0;
 
         for (int i = 0; i < pieces.Length; i++)
         {
@@ -228,24 +208,48 @@ public class Computer : MonoBehaviour
                 Piece currentPiece = pieces[i][j];
                 if (currentPiece != null)
                 {
-                    int pieceValue = GetPieceValue(currentPiece.type) + ((currentPiece.team) ? blackProgressor[i, j] : whiteProgressor[i, j]);
+                    int pieceValue = GetPieceValue(currentPiece, i, j);
                     if (currentPiece.team)
                     {
-                        blackEvaluation += pieceValue;
+                        total += pieceValue;
                     }
                     else
                     {
-                        whiteEvaluation += pieceValue;
+                        total -= pieceValue;
                     }
                 }
             }
         }
-        return blackEvaluation - whiteEvaluation;
+        return total;
     }
 
-private int GetPieceValue(PieceType type)
+private int GetPieceValue(Piece currentPiece, int x, int y)
     {
-        return scores[(int)type];
+        if ((int)currentPiece.type == 0)
+        {
+            return w.scores[(int)currentPiece.type] + ((currentPiece.team) ? w.blackProgressor[x, y] : w.whiteProgressor[x, y]);
+        }
+        else if ((int)currentPiece.type == 1)
+        {
+            return w.scores[(int)currentPiece.type] + ((currentPiece.team) ? w.blackLiberator[x, y] : w.whiteLiberator[x, y]);
+        }
+        else if ((int)currentPiece.type == 2)
+        {
+            return w.scores[(int)currentPiece.type] + w.intellector[x, y];
+        }
+        else if ((int)currentPiece.type == 3)
+        {
+            return w.scores[(int)currentPiece.type] + ((currentPiece.team) ? w.blackDominator[x, y] : w.whiteDominator[x, y]);
+        }
+        else if ((int)currentPiece.type == 4)
+        {
+            return w.scores[(int)currentPiece.type] + w.defensor[x, y];
+        }
+        else if ((int)currentPiece.type == 5)
+        {
+            return w.scores[(int)currentPiece.type] + w.argessor[x, y];
+        }
+        return w.scores[(int)currentPiece.type];
     }
 
     private void DestroyBoard(Board boardCopy)
